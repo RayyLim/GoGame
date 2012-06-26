6505a33d3bd2860bf7fb96710d58b531dc9bc58b
Your Name <Rayylim@gmail.com>
2012-06-26 03:03:40 -0700

    Refactored Code. Extract rules from board. Extracted rules from board into a Rules object

--- GoGameTests/Board.cs
+++ GoGameTests/Board.cs
@@ -2,15 +2,64 @@ namespace GoGameTests
 {
     class Rules
     {
-        
+        private bool inKomiMode = true;
+        private StoneColor currentWinner = StoneColor.White;
+
+        public Board Board { get; set; }
+        public bool IsFullySurroundedBy(int x, int y, StoneColor surroundingStoneColor)
+        {
+            bool surroundedOnLeft = (x == 0 || Board.GetStoneColor(x - 1, y) == surroundingStoneColor);
+            bool surroundedOnRight = Board.GetStoneColor(x + 1, y) == surroundingStoneColor;
+            bool surroundedOnBottom = Board.GetStoneColor(x, y + 1) == surroundingStoneColor;
+            bool surroundedOnTop = (y == 0 || Board.GetStoneColor(x, y - 1) == surroundingStoneColor);
+
+
+            bool fullySurrounded = surroundedOnLeft && surroundedOnRight && surroundedOnTop && surroundedOnBottom;
+            return fullySurrounded;
+        }
+
+        public void CheckStonesAroundPositionAndRemoveIfNeeded(int x, int y)
+        {
+            Board.RemoveStoneIfSurrounded(x, y - 1);
+            Board.RemoveStoneIfSurrounded(x, y + 1);
+            Board.RemoveStoneIfSurrounded(x - 1, y);
+            Board.RemoveStoneIfSurrounded(x + 1, y);
+        }
+
+        public StoneColor GetWinner()
+        {
+            if (inKomiMode)
+            {
+                return StoneColor.White;
+            }
+
+            bool surrounded = IsFullySurroundedBy(1, 1, StoneColor.Black);
+            var color1 = Board.GetStoneColor(1, 2);
+            if (surrounded)
+            {
+                return color1;
+            }
+            return currentWinner;
+        }
+
+
+        public void NotifyStoneAdded(StoneColor stoneColor, int x, int y)
+        {
+            inKomiMode = false;
+            currentWinner = stoneColor;
+        }
     }
 
     public class Board
     {
         private PositionStatus[,] positionStatusMatrix = new PositionStatus[BOARDSIZE,BOARDSIZE];
         private StoneColor[,] stoneColorMatrix = new StoneColor[BOARDSIZE, BOARDSIZE];
-        private StoneColor currentWinner = StoneColor.White;
 
+        public Board()
+        {
+            rules = new Rules {Board = this};
+
+        }
 
         private const int BOARDSIZE = 19;
 
@@ -21,58 +70,33 @@ namespace GoGameTests
 
         public void AddStone(StoneColor stoneColor, int x, int y)
         {
-            inKomiMode = false;
-            currentWinner = stoneColor;
+            rules.NotifyStoneAdded(stoneColor, x, y);
+
             positionStatusMatrix[x, y] = PositionStatus.FilledPosition;
             stoneColorMatrix[x, y] = stoneColor;
 
-            CheckStonesAroundPositionAndRemoveIfNeeded(x, y);
+            rules.CheckStonesAroundPositionAndRemoveIfNeeded(x, y);
         }
 
-        private void CheckStonesAroundPositionAndRemoveIfNeeded(int x, int y)
+        public void RemoveStoneIfSurrounded(int x, int y)
         {
-            RemoveSurroundedStone(x, y - 1);
-            RemoveSurroundedStone(x, y + 1);
-            RemoveSurroundedStone(x - 1, y);
-            RemoveSurroundedStone(x + 1, y);
-        }
-
-        private void RemoveSurroundedStone(int x, int y)
-        {
-            var fullySurrounded = IsFullySurroundedBy(x, y, StoneColor.White);
+            var fullySurrounded = rules.IsFullySurroundedBy(x, y, StoneColor.White);
             if ( fullySurrounded)
             {
                 positionStatusMatrix[x, y] = PositionStatus.EmptyPosition;
             }
         }
 
-        private bool IsFullySurroundedBy(int x, int y, StoneColor surroundingStoneColor)
+        public StoneColor GetStoneColor(int x, int y)
         {
-            bool surroundedOnLeft = (x == 0 || stoneColorMatrix[x - 1, y] == surroundingStoneColor);
-            bool surroundedOnRight = stoneColorMatrix[x + 1, y] == surroundingStoneColor;
-            bool surroundedOnBottom = stoneColorMatrix[x, y + 1] == surroundingStoneColor;
-            bool surroundedOnTop = (y == 0 || stoneColorMatrix[x, y - 1] == surroundingStoneColor);
-
-
-            bool fullySurrounded = surroundedOnLeft && surroundedOnRight && surroundedOnTop && surroundedOnBottom;
-            return fullySurrounded;
+            return stoneColorMatrix[x, y];
         }
 
-        private bool inKomiMode = true;
+        private Rules rules;
+
         public StoneColor GetWinner()
         {
-            if (inKomiMode)
-            {
-                return StoneColor.White;
-            }
-
-            bool surrounded = IsFullySurroundedBy(1, 1, StoneColor.Black);
-            var color1 = stoneColorMatrix[1, 2];
-            if (surrounded)
-            {
-                return color1;
-            }
-            return currentWinner;
+            return rules.GetWinner();
         }
     }
 }
