4e6b672fc2933ca8d070e300a6795708684048cf
royosherove <roy@osherove.com>
2012-07-08 10:11:31 +0300

    iforgot strategy file

---
+++ GoGameTests/WinnerFindingStrategies/WinnerByCountStrategy.cs
@@ -0,0 +1,54 @@
+namespace GoGameTests
+{
+    public class WinnerByCountStrategy : WinnerFindingStrategy
+    {
+        public WinnerByCountStrategy(Board b) : base(b)
+        {
+        }
+
+        public bool IsFullySurroundedBy(int x, int y, StoneColor surroundingStoneColor)
+        {
+            bool surroundedOnLeft = (x < Rules.EDGE || _b.GetStoneColor(x - 1, y) == surroundingStoneColor);
+            bool surroundedOnRight = (x > Board.BOARDSIZE - Rules.EDGE || _b.GetStoneColor(x + 1, y) == surroundingStoneColor);
+            bool surroundedOnBottom = (y > Board.BOARDSIZE - Rules.EDGE || _b.GetStoneColor(x, y + 1) == surroundingStoneColor);
+            bool surroundedOnTop = (y < Rules.EDGE || _b.GetStoneColor(x, y - 1) == surroundingStoneColor);
+
+
+            bool fullySurrounded = surroundedOnLeft && surroundedOnRight && surroundedOnTop && surroundedOnBottom;
+            return fullySurrounded;
+        }
+
+        public override StoneColor GetWinner()
+        {
+            int whiteCells = 0;
+            int blackCells = 0;
+            StoneColor color = StoneColor.Empty;
+            for (int i = 1; i < Board.BOARDSIZE; i++)
+            {
+                for (int j = 1; j < Board.BOARDSIZE; j++)
+                {
+                    if (IsFullySurroundedBy(i, j, StoneColor.White))
+                    {
+                        whiteCells++;
+                    }
+
+                    if (IsFullySurroundedBy(i, j, StoneColor.Black))
+                    {
+                        blackCells++;
+                    }
+                }
+            }
+            if (whiteCells>blackCells)
+            {
+                return StoneColor.White;
+            }
+            
+            if (blackCells>whiteCells)
+            {
+                return StoneColor.Black;
+            }
+            return StoneColor.Empty;
+            
+        }
+    }
+}
