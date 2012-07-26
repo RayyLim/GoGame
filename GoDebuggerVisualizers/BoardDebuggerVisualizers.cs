733f5d88125fa1006c50b1b7ba10fc7213e685c9
Your Name <Rayylim@gmail.com>
2012-07-25 22:14:54 -0700

    Clean up the GoDebuggerVisualizer code

--- GoDebuggerVisualizers/BoardDebuggerVisualizers.cs
+++ GoDebuggerVisualizers/BoardDebuggerVisualizers.cs
@@ -17,8 +17,6 @@ using Microsoft.VisualStudio.DebuggerVisualizers;
 
 namespace GoGameTests
 {
-
-
     public class DebuggerSide : DialogDebuggerVisualizer
     {
         protected override void Show(IDialogVisualizerService windowService,
@@ -27,57 +25,6 @@ namespace GoGameTests
             var board = (objectProvider.GetObject()) as Board;
 
             new GoBoardForm(board).ShowDialog();
-//
-//            StringBuilder sb = new StringBuilder();
-//
-//            sb.AppendLine(" 1234567890123456789");
-//
-//            // Debugger.Launch();
-//            for (int y = 1; y <= Board.BOARDSIZE; y++ )
-//            {
-//                int row = y;
-//                if (row > 9)
-//                    row = row%10;
-//
-//                sb.Append(row);
-//
-//
-//                for(int x = 1; x <= Board.BOARDSIZE; x++)
-//                {
-//                    string stoneString;
-//
-//
-//
-//                    switch(board.GetStoneColor(x, y))
-//                    {
-//                        case StoneColor.Black:
-//                            stoneString = "B";
-//                            break;
-//                        case StoneColor.White:
-//                            stoneString = "W";
-//                            break;
-//                        default:
-//                            stoneString = " ";
-//                            break;
-//                    }
-//                    sb.Append(stoneString);
-//                }
-//                sb.AppendLine();
-//            }
-//            
-//            MessageBox.Show(sb.ToString());
-
-        }
-    }
-
-
-    public class BoardDebuggerVisualizers:DialogDebuggerVisualizer
-    {
-        protected override void Show(IDialogVisualizerService windowService, IVisualizerObjectProvider objectProvider)
-        {
-           var board =  (objectProvider.GetObject()) as Board;
-
-            MessageBox.Show("dude");
 
         }
     }
